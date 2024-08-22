aa85b9b5f6b8e925fd7a7e50707def05a5451731
Rafael Nobre <rafael.nobre.df@gmail.com>
2024-08-22 20:19:27 -0300

    Array

---
+++ src/app/array/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import Pagina from "../components/Pagina";
+
+export default function Array() {
+  return (
+    <Pagina titulo="Página de array">
+          <p>Conteudo da página</p>
+    </Pagina>
+  );
+}
